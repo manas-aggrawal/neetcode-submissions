98058d6ab1ee49341ec5c116a8de2af2479a0e05
Manas Aggrawal <61937093+manas-aggrawal@users.noreply.github.com>
2026-09-09 12:50:37 -0400

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int>um;
+        for(int i=0; i<nums.size(); i++){
+            int diff = target-nums[i];
+            if(um.find(diff)!=um.end()) return {um[diff], i};
+            um.insert({nums[i], i});
+        }
+        return {};
+    }
+};
